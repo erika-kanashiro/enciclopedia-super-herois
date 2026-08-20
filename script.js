9d6efcdc11087acf020b311b40106de2c3888fe2
emilyluciani <emilyluciani2017@gmail.com>
2026-08-19 21:03:55 -0300

    Adicão de código no arquivo Js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-cript.js
+const barraPesquisa = document.getElementById("barraPesquisa");
+const herois = document.querySelectorAll(".heroi");
+
+barraPesquisa.addEventListener("input", function() {
+    const pesquisa = barraPesquisa.value.toLowerCase();
+
+    herois.forEach(function(heroi) {
+        const nome = heroi.textContent.toLowerCase();
+
+        if (nome.includes(pesquisa)) {
+            heroi.style.display = "block";
+        } else {
+            heroi.style.display = "none";
+        }
+    });
+});
